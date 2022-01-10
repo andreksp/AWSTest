84fef815d7db7fca1e040634450e80605e5d08ce
Andre <andreksp@gmail.com>
2022-01-10 11:32:24 -0300

    add missing filese

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 exports.handler = (event, context, callback) => {
-    var currentTime = new time.Date();
-    currentTime.setTimezone("America/Los_Angeles");
     callback(null, {
         statusCode: '200',
         body: 'The time in Sao Paulo is',
